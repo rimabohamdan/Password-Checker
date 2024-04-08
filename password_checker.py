4f22dc24b85344c137a26c18495eb1623f9274de
Rima Bohamdan <151973213+rimabohamdan@users.noreply.github.com>
2024-04-08 21:31:05 +0300

    Create password_checker.py

---
+++ password_checker.py
@@ -0,0 +1,7 @@
+password = input("Enter your password:\n")
+correct_password = "abc"
+if password == correct_password:
+    print("Your password is correct")
+else:
+    print("Your password isn't correct")
+  
